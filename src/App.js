709c3323884e07aed972f0c131f6997a155853ed
Vitor Bruno de Oliveira Barth <vitor.barth@gmail.com>
2021-09-26 13:12:45 -0400

    Created basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { Col, Layout, Row } from 'antd';
+
+import * as d3 from 'd3'
+import './App.css'
+
+const { Header, Content } = Layout;
 
 function App() {
+  useEffect(() => {
+    var svg = d3.select("#circle").append("svg").attr("width", 200).attr("height", 200)
+    svg.append('circle')
+      .attr('cx', 100)
+      .attr('cy', 100)
+      .attr('r', 50)
+      .attr('stroke', 'black')
+      .attr('fill', '#69a3b2');
+
+    return () => {
+      d3.select("#circle svg").remove()
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <h3 style={{ color: 'white' }}>Visualizing Bayesian Networks</h3>``
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content">
+          <Row>
+            <Col xs={24} md={8}>
+              File Input
+            </Col>
+            <Col xs={24} md={16}>
+              <span id="circle" />
+            </Col>
+          </Row>
+        </div>
+      </Content>
+    </Layout>
   );
 }
 
